32f77752baeff7c34bbf9dd46574c8440926bfcd
Tundeh <jamiu4freedom@gmail.com>
2020-12-08 17:55:56 +0100

    add resume button to navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,44 +1,49 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Modal from "../modal";
 import "./index.scss";
 
 const Navbar = (props) => {
-
   const [isShowing, setIsShowing] = useState(false);
 
   const toggle = () => {
-      setIsShowing(!isShowing);
-  }
-
+    setIsShowing(!isShowing);
+  };
 
   const goContact = () => {
-    const anchor = document.querySelector('#contact')
-    anchor.scrollIntoView({ behavior: 'smooth', block: 'center' })
-}
+    const anchor = document.querySelector("#contact");
+    anchor.scrollIntoView({ behavior: "smooth", block: "center" });
+  };
 
+  const goPro = () => {
+    const anchor = document.querySelector("#pro");
+    anchor.scrollIntoView({ behavior: "smooth", block: "center" });
+  };
 
-const goPro = () => {
-  const anchor = document.querySelector('#pro')
-  anchor.scrollIntoView({ behavior: 'smooth', block: 'center' })
-}
+  const goResume = () => {
+    window.open(
+      "https://res.cloudinary.com/team-work/image/upload/v1607444677/portfolio/resume_x51dxs.pdf"
+    );
+  };
   return (
     <div className="navbar">
-    
-    <div className="navbar-wrapper">
-    <div className="navbar-logo">Tundeh</div>
-      <ul className="navbar-links">
-        <li>
-          <span onClick={toggle}>About</span>
-        </li>
-        <li>
-          <span onClick={goPro}>Projects</span>
-        </li>
-        <li>
-          <span onClick={goContact}>Contact</span>
-        </li>
-      </ul>
-    </div>
-    <Modal isShowing={isShowing} handleClick={toggle}></Modal>
+      <div className="navbar-wrapper">
+        <div className="navbar-logo">Tundeh</div>
+        <ul className="navbar-links">
+          <li>
+            <span onClick={toggle}>About</span>
+          </li>
+          <li>
+            <span onClick={goPro}>Projects</span>
+          </li>
+          <li>
+            <span onClick={goContact}>Contact</span>
+          </li>
+          <li>
+            <span onClick={goResume}>Resume</span>
+          </li>
+        </ul>
+      </div>
+      <Modal isShowing={isShowing} handleClick={toggle}></Modal>
     </div>
   );
 };
